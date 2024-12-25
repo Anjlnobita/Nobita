b7669e8c13401f13efd93f9a7e699aab12626045
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-25 20:34:29 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -1,5 +1,4 @@
 import time
-import asyncio
 
 from pyrogram import filters
 from pyrogram.enums import ChatType
@@ -24,9 +23,6 @@ from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-PM_START_TEX = """
-ʜᴇʟʟᴏ `{}`, ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ \nᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ ʙʀᴏ . . . 
-"""
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -88,32 +84,8 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        lol await message.reply_photo(PM_START_TEX.format(message.from_user.mention),)
-        
-    await asyncio.sleep(0.1)
-    await lol.edit_text("🌸")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ.")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ..")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ...")
-    await asyncio.sleep(0.2)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ.")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ..")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ...")
-    await asyncio.sleep(0.2)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ.")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ..")
-    await asyncio.sleep(0.1)
-    await lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ...")
-    await lol.delete()
-    
-    await update.effective_message.reply_photo(
-    photo=config.START_IMG_URL,
+        await message.reply_photo(
+            photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -122,7 +94,6 @@ async def start_pm(client, message: Message, _):
                 chat_id=config.LOGGER_ID,
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
-        
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)

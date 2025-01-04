714e19abb6d4a5c87b1d698dac8262374c44c259
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-04 15:07:32 +0530

    Update search.py

--- AnonXMusic/plugins/tools/search.py
+++ AnonXMusic/plugins/tools/search.py
@@ -4,7 +4,7 @@ import yt_dlp
 import os
 import time
 import threading
-from AnonXMusic.logging import logger 
+from AnonXMusic import logging
 from AnonXMusic import app  # Importing the existing app
 
 
